79b028ff5ffaee0b4bc02bf1745913bf7b92a756
phil-bstorm <philippe.haerens@bstorm.be>
2025-07-14 14:35:10 +0200

    Clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
