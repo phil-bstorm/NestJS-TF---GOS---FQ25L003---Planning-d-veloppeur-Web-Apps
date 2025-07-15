e6aafd5dadd1a390a1cca90aa30f9939ca8d8591
phil-bstorm <philippe.haerens@bstorm.be>
2025-07-15 13:44:42 +0200

    [Add] TypeORM + todo entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './controllers/todo.controller';
 import { TodoService } from './services/todo.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoEntity } from './entities/todo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      database: 'nestjs_todo',
+      username: 'SA',
+      password: 'Some4Complex#Password',
+      options: {
+        encrypt: true,
+        trustServerCertificate: true, // Use true for local development
+      },
+      logging: true,
+      synchronize: true,
+      entities: [TodoEntity],
+    }),
+    TypeOrmModule.forFeature([TodoEntity]),
+  ],
   controllers: [TodoController],
   providers: [TodoService],
 })
